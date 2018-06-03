a301d9e6a0299684a124707aa9bce86b80324059
James Henshaw <jw.henshaw94@gmail.com>
2018-06-03 21:35:41 +0100

    Search input in AppBar with some incomplete styling as I do not yet udnerstand how to fully override Mui styles apperently...

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 // @flow
 import React from 'react';
 
+import { Search as SearchIcon } from '@material-ui/icons';
 import { MuiThemeProvider, withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 
 import theme from './MuiTheme';
+import SearchBar from './components/SearchBar';
+import { POINT_CONVERSION_HYBRID } from 'constants';
 
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-};
+  title: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  searchBar: {},
+});
 
 type Props = {
   classes: Object,
 };
 
-class App extends React.Component<Props> {
+type State = {};
+
+class App extends React.Component<Props, State> {
   render() {
     const { classes } = this.props;
+
     return (
       <MuiThemeProvider theme={theme}>
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
-              <Typography variant="title" color="inherit">
+              <Typography
+                variant="title"
+                color="inherit"
+                className={classes.title}
+              >
                 Project Unicorn
               </Typography>
+              <div className={classes.searchBar}>
+                <SearchBar onClose={this.hideSearchBar} />
+              </div>
             </Toolbar>
           </AppBar>
         </div>
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,76 @@
+// @flow
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => {
+  const bottomLineColor = theme.palette.secondary.main;
+
+  return {
+    root: {},
+    textField: {
+      width: 300,
+    },
+    textInputColor: {
+      color: theme.palette.secondary.main,
+      '&&focused': {
+        color: theme.palette.secondary.main,
+      },
+    },
+    textInputUnderline: {
+      '&:hover:not($disabled):not($error):not($focused):before': {
+        // doesn't override hove state..why?
+        borderBottomColor: bottomLineColor,
+      },
+      '&:after': {
+        borderBottomColor: bottomLineColor,
+      },
+      '&:before': {
+        borderBottomColor: bottomLineColor,
+      },
+      '&$disabled:before': {
+        borderBottomColor: bottomLineColor,
+      },
+    },
+  };
+};
+
+class SearchBar extends React.Component<Props, State> {
+  state = {
+    searchText: '',
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="searchText"
+          label="Search"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange('searchText')}
+          InputProps={{
+            classes: {
+              input: classes.textInputColor,
+              underline: classes.textInputUnderline,
+            },
+          }}
+          InputLabelProps={{
+            classes: {
+              root: classes.textInputColor,
+            },
+          }}
+        />
+      </form>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchBar);
